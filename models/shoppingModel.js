93938fbd59682a6bc8b87620d011f0f68b4a179d
mosaab-rady <mosp.rady@gmail.com>
2021-09-20 22:04:18 +0200

    added size and dileverd to shopping model

--- models/shoppingModel.js
+++ models/shoppingModel.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
+const AppError = require('../utils/AppError');
 const Schema = mongoose.Schema;
+const Product = require('./productModel');
 
 const shoppingSchema = new mongoose.Schema({
   product: {
@@ -24,9 +26,54 @@ const shoppingSchema = new mongoose.Schema({
     type: Number,
     required: [true, 'Shopping must have a price.'],
   },
+  size: {
+    type: String,
+    enum: ['small', 'medium', 'large', 'xLarge', 'xxLarge'],
+  },
+  delivered: {
+    type: Boolean,
+    default: false,
+  },
 });
 
 shoppingSchema.index({ product: 1 });
+shoppingSchema.index({ user: 1 });
+
+shoppingSchema.pre('save', async function (next) {
+  // 1) find the product
+  const product = await Product.findById(this.product).select('size');
+  console.log(product);
+  // 2) sheck if the size is available
+  if (this.size === 'small') {
+    if (product.size.small === 0) {
+      return next(new AppError('This size is not available now.', 400));
+    } else {
+      return next();
+    }
+  } else if (this.size === 'medium') {
+    if (product.size.medium === 0) {
+      return next(new AppError('This size is not available now.', 400));
+    } else {
+      return next();
+    }
+  } else if (this.size === 'large') {
+    if (product.size.large === 0) {
+      return next(new AppError('This size is not available now.', 400));
+    }
+  } else if (this.size === 'xLarge') {
+    if (product.size.xLarge === 0) {
+      return next(new AppError('This size is not available now.', 400));
+    } else {
+      return next();
+    }
+  } else if (this.size === 'xxLarge') {
+    if (product.size.xxLarge === 0) {
+      return next(new AppError('This size is not available now.', 400));
+    } else {
+      return next();
+    }
+  }
+});
 
 const Shopping = mongoose.model('Shopping', shoppingSchema);
 
