2db9998372cd5e59936005492957258dede12b5c
mosaab-rady <mosp.rady@gmail.com>
2021-11-14 21:20:26 +0200

    helmet

--- app.js
+++ app.js
@@ -32,7 +32,14 @@ if (process.env.NODE_ENV === 'production') {
 app.options('*', cors());
 
 // Set security HTTP headers
-app.use(helmet());
+app.use(
+  helmet.contentSecurityPolicy({
+    useDefaults: true,
+    directives: {
+      'script-src': ["'unsafe-inline'"],
+    },
+  })
+);
 
 if (process.env.NODE_ENV === 'development') {
   const morgan = require('morgan');
