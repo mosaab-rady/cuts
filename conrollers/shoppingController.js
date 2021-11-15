27111d651b69b0fd4dc416d2595c7ea700370b66
mosaab-rady <mosp.rady@gmail.com>
2021-11-15 20:03:36 +0200

    stripe

--- conrollers/shoppingController.js
+++ conrollers/shoppingController.js
@@ -50,7 +50,7 @@ exports.webhookCheckout = (req, res, next) => {
   }
 
   if (event.type === 'checkout.session.completed') {
-    console.log(event.data.object);
+    console.log(event.data);
     // const session = await stripe.checkout.sessions.retrieve(
     //   event.data.object.id,
     //   {
