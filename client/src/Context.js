eb8c992322ed422e063f1c14d5a1d748db02eb11
mosaab-rady <mosp.rady@gmail.com>
2021-11-15 13:57:16 +0200

    context

--- client/src/Context.js
+++ client/src/Context.js
@@ -14,12 +14,11 @@ const reducer = (state, action) => {
     const order = action.payload;
     let orders = [...state.shoppings];
 
-    console.log('out', order);
-
     for (let i = 0; i < state.shoppings.length; i++) {
       if (order.id === orders[i].id && order.size === orders[i].size) {
-        console.log('inside if', order);
+        console.log('1', orders[i].quantity);
         orders[i] = { ...orders[i], quantity: orders[i].quantity++ };
+        console.log('2', orders[i].quantity);
         orders[i] = {
           ...orders[i],
           total: orders[i].price * orders[i].quantity,
