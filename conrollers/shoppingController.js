5c65565adebea3b55e0b002fe90f9585883b6b59
mosaab-rady <mosp.rady@gmail.com>
2021-11-16 11:44:32 +0200

    stripe

--- conrollers/shoppingController.js
+++ conrollers/shoppingController.js
@@ -12,11 +12,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
       amount: item.price * 100,
       currency: 'usd',
       quantity: item.quantity,
-      description: JSON.stringify({
-        name: item.name,
-        color: item.color,
-        size: item.size,
-      }),
+      description: `${item.color} ${item.name} size: ${item.size} `,
       // images: [
       //   `${req.protocol}://${req.get('host')}/api/v1/images/${item.image}`,
       // ],
@@ -25,6 +21,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 
   let session = await stripe.checkout.sessions.create({
     line_items: items,
+    client_reference_id: JSON.stringify(products),
     payment_method_types: ['card'],
     mode: 'payment',
     cancel_url: `${req.protocol}://${req.get('host')}`,
@@ -56,14 +53,7 @@ exports.webhookCheckout = catchAsync(async (req, res, next) => {
   }
 
   if (event.type === 'checkout.session.completed') {
-    const session = await stripe.checkout.sessions.retrieve(
-      event.data.object.id,
-      {
-        expand: ['line_items'],
-      }
-    );
-
-    console.log(session.line_items);
+    console.log(event);
   }
 
   res.status(200).json({ received: true });
