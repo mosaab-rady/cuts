7dc4a5d4353315391a11be5889061f00ad851e79
mosaab-rady <mosp.rady@gmail.com>
2021-11-16 11:51:20 +0200

    stripe

--- conrollers/shoppingController.js
+++ conrollers/shoppingController.js
@@ -21,7 +21,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 
   let session = await stripe.checkout.sessions.create({
     line_items: items,
-    client_reference_id: JSON.stringify(products),
+    metadata: products,
     payment_method_types: ['card'],
     mode: 'payment',
     cancel_url: `${req.protocol}://${req.get('host')}`,
