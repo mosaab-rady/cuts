ebce6fd4e60312a958216e207fb678f82acd2cb9
mosaab-rady <mosp.rady@gmial.com>
2022-06-03 23:30:13 +0200

    update image controller & class

--- conrollers/ImageController.js
+++ conrollers/ImageController.js
@@ -26,9 +26,9 @@ exports.multerUploadImages = upload.fields([
   { name: 'images', maxCount: 3 },
 ]);
 
-const resizeImage = async (buffer, height, width) => {
+const resizeImage = async ({ buffer, height, width }) => {
   return await sharp(buffer)
-    .resize(height, width)
+    .resize(width, height)
     .toFormat('jpeg')
     .jpeg({ quality: 100 })
     .toBuffer();
@@ -40,84 +40,99 @@ exports.resizeImageAndUploadToS3 = catchAsync(async (req, res, next) => {
 
   if (req.files.image_cover) {
     // resize using sharp
-    const buffer = await resizeImage(
-      req.files.image_cover[0].buffer,
-      1500,
-      300
-    );
+    const buffer = await resizeImage({
+      buffer: req.files.image_cover[0].buffer,
+      width: 1500,
+      height: 300,
+    });
 
     const fileStream = Readable.from(buffer);
     //  upload to s3
-    req.body.image_cover = await new Image(
-      `image_cover_${Date.now()}.jpeg`,
-      fileStream
-    ).upload();
+    req.body.image_cover = await new Image().upload({
+      body: fileStream,
+      key: `image_cover_${Date.now()}.jpeg`,
+    });
   }
 
   if (req.files.image_hero) {
-    const buffer = await resizeImage(req.files.image_hero[0].buffer, 1500, 600);
+    const buffer = await resizeImage({
+      buffer: req.files.image_hero[0].buffer,
+      width: 1500,
+      height: 600,
+    });
 
     const fileStream = Readable.from(buffer);
 
-    req.body.image_hero = await new Image(
-      `image_hero_${Date.now()}.jpeg`,
-      fileStream
-    ).upload();
+    req.body.image_hero = await new Image().upload({
+      body: fileStream,
+      key: `image_hero_${Date.now()}.jpeg`,
+    });
   }
 
   // image for section
   if (req.files.image) {
-    const buffer = await resizeImage(req.files.image[0].buffer, 1500, 1500);
+    const buffer = await resizeImage({
+      buffer: req.files.image[0].buffer,
+      width: 1500,
+      height: 1500,
+    });
 
     const fileStream = Readable.from(buffer);
 
-    req.body.image = await new Image(
-      `image_${Date.now()}.jpeg`,
-      fileStream
-    ).upload();
+    req.body.image = await new Image().upload({
+      body: fileStream,
+      key: `image_${Date.now()}.jpeg`,
+    });
   }
 
   // image for navbar
   if (req.files.image_detail) {
-    const buffer = await resizeImage(
-      req.files.image_detail[0].buffer,
-      1000,
-      1200
-    );
+    const buffer = await resizeImage({
+      buffer: req.files.image_detail[0].buffer,
+      width: 1000,
+      height: 1200,
+    });
 
     const fileStream = Readable.from(buffer);
 
-    req.body.image_detail = await new Image(
-      `image_detail_${Date.now()}.jpeg`,
-      fileStream
-    );
+    req.body.image_detail = await new Image().upload({
+      body: fileStream,
+      key: `image_detail_${Date.now()}.jpeg`,
+    });
   }
 
   // image for the shop page
   if (req.files.image_overview) {
-    const buffer = await resizeImage(
-      req.files.image_overview[0].buffer,
-      750,
-      900
-    );
+    const buffer = await resizeImage({
+      buffer: req.files.image_overview[0].buffer,
+      width: 750,
+      height: 900,
+    });
 
     const fileStream = Readable.from(buffer);
 
-    req.body.image_overview = await new Image(
-      `image_overview_${Date.now()}.jpeg`,
-      fileStream
-    ).upload();
+    req.body.image_overview = await new Image().upload({
+      body: fileStream,
+      key: `image_overview_${Date.now()}.jpeg`,
+    });
   }
 
   if (req.files.images) {
     const images = [];
     for (let i = 0; i < req.files.images.length; i++) {
-      const buffer = await resizeImage(req.files.images[i].buffer, 1500, 1500);
+      const buffer = await resizeImage({
+        buffer: req.files.images[i].buffer,
+        width: 1500,
+        height: 1500,
+      });
 
       const fileStream = Readable.from(buffer);
 
       images.push(
-        await new Image(`product_image_${Date.now()}.jpeg`, fileStream).upload()
+        await new Image().upload({
+          key: `product_image_${Date.now()}.jpeg`,
+          body: fileStream,
+        })
       );
     }
     req.body.images = images;
--- utils/image.js
+++ utils/image.js
@@ -1,35 +1,31 @@
 const { S3 } = require('aws-sdk');
 
 module.exports = class Image {
-  constructor(key, body) {
-    this.key = key;
-    this.body = body;
-  }
-
   s3() {
     return new S3({
       region: process.env.AWS_BUCKET_REGION,
     });
   }
 
-  async upload() {
+  async upload({ body, key }) {
     const res = await this.s3()
       .upload({
         Bucket: process.env.AWS_BUCKET_NAME,
-        Body: this.body,
-        Key: this.key,
+        Body: body,
+        Key: key,
         ContentType: 'image/jpeg',
       })
       .promise();
 
+    console.log(res.Location);
     return res.Location;
   }
 
-  async delete() {
+  async delete({ key }) {
     await this.s3()
       .deleteObject({
         Bucket: process.env.AWS_BUCKET_NAME,
-        Key: this.key,
+        Key: key,
       })
       .promise();
   }
